48b9e7fd36ef1acfe0eef7ff451c20b5d0e0b60e
ZJamm1993 <670231925@qq.com>
2017-07-28 09:48:32 +0800

    Initial Commit

---
+++ Watch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Watch
+//
+//  Created by jam on 17/7/28.
+//  Copyright © 2017年 jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
